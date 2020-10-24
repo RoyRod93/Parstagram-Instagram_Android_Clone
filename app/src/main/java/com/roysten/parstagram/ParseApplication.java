5ffa2c1809bab73e866f7fa9873e90d6173c1493
RoyRod93 <rroysten@gmail.com>
2020-10-23 20:02:18 -0500

    Added functionality for Image Capture and save in device, post images with decription.

--- app/src/main/java/com/roysten/parstagram/ParseApplication.java
+++ app/src/main/java/com/roysten/parstagram/ParseApplication.java
@@ -3,11 +3,15 @@ package com.roysten.parstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        //Register your Parse Model
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("mkn6DabHFTywtpyz57NwGrRBN3CQYGls6GS6fCDZ")
